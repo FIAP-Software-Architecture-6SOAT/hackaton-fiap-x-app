8fd9c64e1cc7944549e328b82c05a2a57c24eaa2
Danilo Cassola <danilocassola@gmail.com>
2025-01-28 22:43:21 -0300

    chore: add Authorization Header Bearer

--- src/adapters/middlewares/authenticationMiddleware.ts
+++ src/adapters/middlewares/authenticationMiddleware.ts
@@ -6,10 +6,26 @@ import type { AuthenticationUseCase } from '@/application/usecases';
 export const authenticationMiddleware =
   (authenticationUseCase: AuthenticationUseCase) =>
   async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
-    const token = request.headers.authorization;
-    if (!token) return reply.status(401).send({ message: 'Unauthorized' });
+    const { authorization } = request.headers;
 
-    const payload = await authenticationUseCase.authenticate(token);
+    if (!authorization)
+      return reply
+        .status(401)
+        .send({ message: 'Missing Authorization Header' });
+
+    const [schema, token] = authorization.split(' ');
+
+    if (!schema || !token) {
+      return reply
+        .status(401)
+        .send({ message: 'Invalid Authorization Header' });
+    }
+
+    if (schema !== 'Bearer') {
+      return reply.status(401).send({ message: 'Invalid schema' });
+    }
+
+    const payload = authenticationUseCase.authenticate(token);
     if (!payload) return reply.status(401).send({ message: 'Unauthorized' });
 
     Object.assign(request, {
